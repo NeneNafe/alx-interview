079ad5da6e1fe3b5ebca45146d36ec97851bef09
NeneNafe <nenechiwaya@gmail.com>
2024-04-12 15:10:05 +0200

    Corrections

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -13,7 +13,7 @@ def pascal_triangle(n):
     if (n <= 0):
         return []
 
-    triangle = [[]]
+    triangle = []
     for rownum in range(n):
         newValue = 1
         printlist = [newValue]
