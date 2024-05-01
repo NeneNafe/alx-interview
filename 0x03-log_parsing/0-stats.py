6879a2158777641f5f31dc423a43ab3b243d5566
NeneNafe <nenechiwaya@gmail.com>
2024-05-01 23:10:31 +0200

    Corrections

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,10 +4,11 @@
 import sys
 
 
-total_size = 0
+# a count of all status code in a dict presentation
 lines_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
               '404': 0, '405': 0, '500': 0}
 line_count = 0
+total_size = 0  # keeps the count of the numbs counted
 
 
 try:
@@ -18,17 +19,22 @@ try:
             status_code = line_list[-2]
             file_size = int(line_list[-1])
 
+            # checks whether the code status exists in the dict
+            # and increments its count
             if status_code in lines_dict.keys():
                 lines_dict[status_code] += 1
 
+            # updates the total size
             total_size += file_size
 
+            # updates the count of lines
             line_count += 1
 
             if line_count == 10:
-                line_count = 0
+                line_count = 0  # resets count
                 print('File size: {}'.format(total_size))
 
+                # prints out the status code count
                 for key, value in sorted(lines_dict.items()):
                     if value != 0:
                         print('{}: {}'.format(key, value))
